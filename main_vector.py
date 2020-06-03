7b817cb32acf8e0e94cc99efaf491e3a5d242194
dkzhang <dk@home.pc>
2020-06-03 22:12:22 +0800

    init

---
+++ main_vector.py
@@ -0,0 +1,17 @@
+from palyLA.Vector import Vector
+
+if __name__ == "__main__":
+    vec1 = Vector([5, 2])
+    print(vec1)
+    print(len(vec1))
+    print("vec[0] = {}, vec[1] = {}".format(vec1[0], vec1[1]))
+
+    vec2 = Vector([3, 1])
+    print("{} + {} = {}".format(vec1, vec2, vec1 + vec2))
+    print("{} - {} = {}".format(vec1, vec2, vec1 - vec2))
+
+    print("{} * {} = {}".format(vec1, 3, vec1 * 3))
+    print("{} * {} = {}".format(3, vec1, 3 * vec1))
+
+    print("+{} = {}".format(vec1, +vec1))
+    print("-{} = {}".format(vec1, -vec1))
