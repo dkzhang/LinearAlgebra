51f5678a741e30765f9c61a13d54783f8be166db
dkzhang <dk@home.pc>
2020-06-14 11:18:28 +0800

    update

--- main_vector.py
+++ main_vector.py
@@ -1,21 +1,21 @@
 from palyLA.Vector import Vector
 
 if __name__ == "__main__":
-    vec1 = Vector([5, 2])
-    print(vec1)
-    print(len(vec1))
-    print("vec[0] = {}, vec[1] = {}".format(vec1[0], vec1[1]))
+    vec = Vector([5, 2])
+    print(vec)
+    print(len(vec))
+    print("vec[0] = {}, vec[1] = {}".format(vec[0], vec[1]))
 
     vec2 = Vector([3, 1])
-    print("{} + {} = {}".format(vec1, vec2, vec1 + vec2))
-    print("{} - {} = {}".format(vec1, vec2, vec1 - vec2))
+    print("{} + {} = {}".format(vec, vec2, vec + vec2))
+    print("{} - {} = {}".format(vec, vec2, vec - vec2))
 
-    print("{} * {} = {}".format(vec1, 3, vec1 * 3))
-    print("{} * {} = {}".format(3, vec1, 3 * vec1))
+    print("{} * {} = {}".format(vec, 3, vec * 3))
+    print("{} * {} = {}".format(3, vec, 3 * vec))
 
-    print("+{} = {}".format(vec1, +vec1))
-    print("-{} = {}".format(vec1, -vec1))
+    print("+{} = {}".format(vec, +vec))
+    print("-{} = {}".format(vec, -vec))
 
     zero2 = Vector.zero(2)
     print(zero2)
-    print("{} + {} = {}".format(vec1, zero2, vec1 + zero2))
+    print("{} + {} = {}".format(vec, zero2, vec + zero2))
